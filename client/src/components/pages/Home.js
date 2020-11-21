db072b949c0922563556f94438efa630fc7bbb5b
Brian Boehm <brianboehm.web@gmail.com>
2020-11-21 18:04:45 +0100

    added empty svg to featured offers & featured categories in case of empty in home component

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -13,9 +13,13 @@ import CardMediaV1 from '../layout/CardMediaV1';
 import CardMediaV2 from '../layout/CardMediaV2';
 // Assets
 import LoadingGif from '../../assets/loading-transparent.gif';
+import EmptySvg from '../../assets/undraw/empty.svg';
 
 // define styles
 const useStyles = makeStyles(theme => ({
+	home: {
+		padding: theme.spacing(0, 0, 8)
+	},
 	h2: {
 		fontSize: '1.7rem',
 		fontWeight: 700,
@@ -26,6 +30,10 @@ const useStyles = makeStyles(theme => ({
 	},
 	loadingGif: {
 		height: '20vh'
+	},
+	emptySvg: {
+		height: '200px',
+		marginBottom: theme.spacing(2)
 	},
 	motionDiv: {
 		height: '100%'
@@ -78,7 +86,7 @@ const Home = () => {
 	}, []);
 
 	return (
-		<div className='home'>
+		<div className={classes.home}>
 			<Hero />
 			<Container className={classes.categories} maxWidth='lg'>
 				{loading ? (
@@ -91,127 +99,141 @@ const Home = () => {
 							<Typography className={classes.h2} variant='h2'>
 								Featured <span className={classes.textPrimary}>Categories</span>
 							</Typography>
-							<Grid container width='100%' spacing={4}>
-								{categories.map(category => {
-									return (
-										<Grid key={category._id} item xs={12} sm={6} md={3}>
-											<motion.div
-												className={classes.motionDiv}
-												transition={{
-													duration: 1,
-													type: 'tween'
-												}}
-												initial={{ opacity: 0 }}
-												animate={{ opacity: 1 }}
-											>
-												<CardMediaV1
-													link={`/offers?id=${category._id}`}
-													image={category.image}
-													title={category.title}
-													btnname='Explorer'
-													btnicon={<ArrowRightIcon />}
+							{categories ? (
+								<Grid container width='100%' spacing={4}>
+									{categories.map(category => {
+										return (
+											<Grid key={category._id} item xs={12} sm={6} md={3}>
+												<motion.div
+													className={classes.motionDiv}
+													transition={{
+														duration: 1,
+														type: 'tween'
+													}}
+													initial={{ opacity: 0 }}
+													animate={{ opacity: 1 }}
 												>
-													<Typography className={classes.cardParagraph} variant='body1'>
-														{category.description}
-													</Typography>
-												</CardMediaV1>
-											</motion.div>
-										</Grid>
-									);
-								})}
-							</Grid>
+													<CardMediaV1
+														link={`/offers?id=${category._id}`}
+														image={category.image}
+														title={category.title}
+														btnname='Explorer'
+														btnicon={<ArrowRightIcon />}
+													>
+														<Typography className={classes.cardParagraph} variant='body1'>
+															{category.description}
+														</Typography>
+													</CardMediaV1>
+												</motion.div>
+											</Grid>
+										);
+									})}
+								</Grid>
+							) : (
+								<Box width='100%' textAlign='center'>
+									<img className={classes.emptySvg} src={EmptySvg} alt='Empty' />
+									<Typography variant='body1'>Sorry there aren't any categories yet...</Typography>
+								</Box>
+							)}
 						</Box>,
 						<br key='break-1' />,
 						<Box key='offers-box' width='100%'>
 							<Typography className={classes.h2} variant='h2'>
 								Featured <span className={classes.textPrimary}>Offers</span>
 							</Typography>
-							<Grid container width='100%' spacing={4}>
-								<Grid key={offers[0]._id} item xs={12} md={6}>
-									<motion.div
-										className={classes.motionDiv}
-										transition={{
-											duration: 1,
-											type: 'tween'
-										}}
-										initial={{ opacity: 0 }}
-										animate={{ opacity: 1 }}
-									>
-										<CardMediaV2
-											price={`Daily Price: ${offers[0].price}`}
-											link={`/offer?id=${offers[0]._id}`}
-											image={`${offers[0].images[0]}`}
-											title={offers[0].title}
-											btnname='View'
-											btnicon={<ArrowRightIcon />}
+							{offers ? (
+								<Grid container width='100%' spacing={4}>
+									<Grid key={offers[0]._id} item xs={12} md={6}>
+										<motion.div
+											className={classes.motionDiv}
+											transition={{
+												duration: 1,
+												type: 'tween'
+											}}
+											initial={{ opacity: 0 }}
+											animate={{ opacity: 1 }}
 										>
-											<Typography className={classes.cardParagraph} variant='body1'>
-												{offers[0].description.join(' ').length > 500 ? (
-													offers[0].description.join(' ').substring(0, 500) + '...'
-												) : (
-													offers[0].description.join(' ')
-												)}
-											</Typography>
-										</CardMediaV2>
-									</motion.div>
-								</Grid>
-								<Grid key={offers[1]._id} item xs={12} md={3}>
-									<motion.div
-										className={classes.motionDiv}
-										transition={{
-											duration: 1,
-											type: 'tween'
-										}}
-										initial={{ opacity: 0 }}
-										animate={{ opacity: 1 }}
-									>
-										<CardMediaV2
-											price={`Daily Price: ${offers[1].price}`}
-											link={`/offer?id=${offers[1]._id}`}
-											image={`${offers[1].images[0]}`}
-											title={offers[1].title}
-											btnname='View'
-											btnicon={<ArrowRightIcon />}
+											<CardMediaV2
+												price={`Daily Price: ${offers[0].price}`}
+												link={`/offer?id=${offers[0]._id}`}
+												image={`${offers[0].images[0]}`}
+												title={offers[0].title}
+												btnname='View'
+												btnicon={<ArrowRightIcon />}
+											>
+												<Typography className={classes.cardParagraph} variant='body1'>
+													{offers[0].description.join(' ').length > 500 ? (
+														offers[0].description.join(' ').substring(0, 500) + '...'
+													) : (
+														offers[0].description.join(' ')
+													)}
+												</Typography>
+											</CardMediaV2>
+										</motion.div>
+									</Grid>
+									<Grid key={offers[1]._id} item xs={12} md={3}>
+										<motion.div
+											className={classes.motionDiv}
+											transition={{
+												duration: 1,
+												type: 'tween'
+											}}
+											initial={{ opacity: 0 }}
+											animate={{ opacity: 1 }}
 										>
-											<Typography className={classes.cardParagraph} variant='body1'>
-												{offers[1].description.join(' ').length > 150 ? (
-													offers[1].description.join(' ').substring(0, 150) + '...'
-												) : (
-													offers[1].description.join(' ')
-												)}
-											</Typography>
-										</CardMediaV2>
-									</motion.div>
-								</Grid>
-								<Grid key={offers[2]._id} item xs={12} md={3}>
-									<motion.div
-										className={classes.motionDiv}
-										transition={{
-											duration: 1,
-											type: 'tween'
-										}}
-										initial={{ opacity: 0 }}
-										animate={{ opacity: 1 }}
-									>
-										<CardMediaV2
-											price={`Daily Price: ${offers[2].price}`}
-											link={`/offer?id=${offers[2]._id}`}
-											image={`${offers[2].images[0]}`}
-											title={offers[2].title}
-											btnname='View'
-											btnicon={<ArrowRightIcon />}
+											<CardMediaV2
+												price={`Daily Price: ${offers[1].price}`}
+												link={`/offer?id=${offers[1]._id}`}
+												image={`${offers[1].images[0]}`}
+												title={offers[1].title}
+												btnname='View'
+												btnicon={<ArrowRightIcon />}
+											>
+												<Typography className={classes.cardParagraph} variant='body1'>
+													{offers[1].description.join(' ').length > 150 ? (
+														offers[1].description.join(' ').substring(0, 150) + '...'
+													) : (
+														offers[1].description.join(' ')
+													)}
+												</Typography>
+											</CardMediaV2>
+										</motion.div>
+									</Grid>
+									<Grid key={offers[2]._id} item xs={12} md={3}>
+										<motion.div
+											className={classes.motionDiv}
+											transition={{
+												duration: 1,
+												type: 'tween'
+											}}
+											initial={{ opacity: 0 }}
+											animate={{ opacity: 1 }}
 										>
-											<Typography className={classes.cardParagraph} variant='body1'>
-												{offers[2].description.join(' ').length > 150 ? (
-													offers[2].description.join(' ').substring(0, 150) + '...'
-												) : (
-													offers[2].description.join(' ')
-												)}
-											</Typography>
-										</CardMediaV2>
-									</motion.div>
+											<CardMediaV2
+												price={`Daily Price: ${offers[2].price}`}
+												link={`/offer?id=${offers[2]._id}`}
+												image={`${offers[2].images[0]}`}
+												title={offers[2].title}
+												btnname='View'
+												btnicon={<ArrowRightIcon />}
+											>
+												<Typography className={classes.cardParagraph} variant='body1'>
+													{offers[2].description.join(' ').length > 150 ? (
+														offers[2].description.join(' ').substring(0, 150) + '...'
+													) : (
+														offers[2].description.join(' ')
+													)}
+												</Typography>
+											</CardMediaV2>
+										</motion.div>
+									</Grid>
 								</Grid>
-							</Grid>
+							) : (
+								<Box width='100%' textAlign='center'>
+									<img className={classes.emptySvg} src={EmptySvg} alt='Empty' />
+									<Typography variant='body1'>Sorry there aren't any offers yet...</Typography>
+								</Box>
+							)}
 						</Box>
 					]
 				)}
