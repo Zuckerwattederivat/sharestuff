76e7369b4e4f731777a7e3a44da2588d9dfde3d0
Brian Boehm <brianboehm.web@gmail.com>
2020-12-13 00:03:56 +0100

    booking backend  completed

--- routes/bookings.js
+++ routes/bookings.js
@@ -38,8 +38,8 @@ router.post('/create', [ auth, check('offerId', 'No offer was given ').notEmpty(
 
 			// if offer could not be updated
 			if (updatedOffer.nModified !== 1) {
-				return res.status(500).json({ msg: 'Offer could not be set to booked' });
-				// if offer update was successfull
+				return res.status(500).json({ msg: 'Offer could not be booked' });
+				// if offer update was successful
 			} else {
 				// create booking
 				const booking = new Booking({
@@ -54,21 +54,106 @@ router.post('/create', [ auth, check('offerId', 'No offer was given ').notEmpty(
 				// send response
 				if (payload) {
 					return res.status(200).json(payload);
-
 					// reset offer and send response
 				} else {
 					await Offer.updateOne({ _id: ObjectId(offerId) }, { active: true });
 					return res.status(500).json({ msg: 'Booking could not be created' });
 				}
 			}
+
 			// if offer doesn't exists
 		} else {
 			return res.status(400).json({ msg: 'Offer does not exist' });
 		}
 	} catch (err) {
 		console.error(err.message);
-		res.status(500).json({ msg: 'Server Error' });
-		res.status(500).send('Server error');
+		Offer.updateOne({ _id: ObjectId(offerId) }, { active: true })
+			.then(() => {
+				res.status(500).json({ msg: 'Server Error' });
+				res.status(500).send('Server error');
+			})
+			.catch(err => {
+				console.error(err.message);
+				res.status(500).json({ msg: 'Server Error - Could not reset offer after error occured' });
+				res.status(500).send('Server Error - Could not reset offer after error occured');
+			});
+	}
+});
+
+// @route     POST api/bookings/delete
+// @desc      Delete booking
+// @access    Private
+router.delete('/delete', [ auth, check('bookingId', 'No Booking was given ').notEmpty() ], async (req, res) => {
+	// check if validation errors exist and response with 400 if true
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(400).json({ errors: errors.array() });
+	}
+
+	// get paramters
+	const { bookingId } = req.body;
+
+	try {
+		// get booking
+		const booking = await Booking.findById(bookingId);
+
+		// if booking exists
+		if (booking) {
+			// cehck if user is authorized to delete the booking
+			if (booking.bookedBy === req.user.id || booking.createdBy === req.user.id) {
+				// find offer
+				const offer = await Offer.findById(booking.offerId);
+
+				// if offer exists
+				if (offer) {
+					// set offer active
+					const updatedOffer = await Offer.updateOne({ _id: ObjectId(booking.offerId) }, { active: true });
+
+					// if offer could not be updated
+					if (updatedOffer.nModified !== 1) {
+						return res.status(500).json({ msg: 'Offer could not be unbooked' });
+						// if offer update was successful
+					} else {
+						// remove booking
+						const payload = await Booking.find({ _id: bookingId }).remove().exec();
+
+						// send response
+						if (payload.deletedCount === 1) {
+							return res.status(200).json({ msg: 'Booking deleted' });
+							// reset offer and send response
+						} else {
+							await Offer.updateOne({ _id: ObjectId(booking.offerId) }, { active: false });
+							return res.status(500).json({ msg: 'Booking could not be deleted' });
+						}
+					}
+
+					// if offer doesn't exist
+				} else {
+					return res.status(400).json({ msg: 'Offer does not exist' });
+				}
+
+				// if user is unauthorized to delete the offer respond with 401
+			} else {
+				return res.status(401).json({ msg: 'Authorization denied' });
+			}
+
+			// if offer doesn't exists
+		} else {
+			return res.status(400).json({ msg: 'Booking does not exist' });
+		}
+	} catch (err) {
+		console.error(err.message);
+		const booking = await Booking.findById(bookingId);
+		Offer.updateOne({ _id: ObjectId(booking.offerId) }, { active: false })
+			.then(() => {
+				res.status(500).json({ msg: 'Server Error' });
+				res.status(500).send('Server error');
+			})
+			.catch(err => {
+				console.error(err.message);
+				res.status(500).json({ msg: 'Server Error - Could not reset offer after error occured' });
+				res.status(500).send('Server Error - Could not reset offer after error occured');
+			});
 	}
 });
 
