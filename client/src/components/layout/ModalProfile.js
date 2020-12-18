4a6976400659776bbde7b00eb64f7f53ccfe8798
Brian Boehm <brianboehm.web@gmail.com>
2020-12-18 05:03:43 +0100

    created modalProfile.js

---
+++ client/src/components/layout/ModalProfile.js
@@ -0,0 +1,10 @@
+// Node Modules
+import React from 'react';
+
+// ModalProfile Component
+const ModalProfile = () => {
+	return <div>Modal Profile</div>;
+};
+
+// export ModalProfile Component
+export default ModalProfile;
